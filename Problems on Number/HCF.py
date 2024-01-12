61aa9547bf7d82e343d398883a356a2f06592dc0
Srajal Sawner <90255417+Srajal16@users.noreply.github.com>
2024-01-12 15:27:10 +0530

    Add files via upload

---
+++ Problems on Number/HCF.py
@@ -0,0 +1,17 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+num1 = 36
+num2 = 60
+
+while num1 != num2:
+     if num1 > num2:
+         num1 = num1 - num2
+     else:
+         num2 = num2 - num1
+print("HCF: ",num1)
