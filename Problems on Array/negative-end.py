9642121c1eaad0c02bf9c9d5a9cb666601ab9cb0
Srajal Sawner <90255417+Srajal16@users.noreply.github.com>
2024-01-19 18:14:25 +0530

    Add files via upload

---
+++ Problems on Array/negative-end.py
@@ -0,0 +1,28 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def rearrange(arr, n ) : 
+  
+    # Please refer partition() in  
+    # below post 
+    # https://www.geeksforgeeks.org / quick-sort / j = 0 
+    j = 0 
+    for i in range(0, n) : 
+        if (arr[i] < 0) : 
+            temp = arr[i] 
+            arr[i] = arr[j] 
+            arr[j]= temp 
+            j = j + 1
+    print(arr) 
+  
+# Driver code 
+arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9] 
+n = len(arr) 
+rearrange(arr, n) 
+
+
+
